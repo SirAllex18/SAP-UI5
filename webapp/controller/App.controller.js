b6c19d335f7a912e88a14ce32fac3517801a7295
Alexandru <anistor35@yahoo.com>
2023-09-08 17:06:57 +0300

    Added i18n files and implemented reading messages

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast", // Modules part (used to display the message at the bottom of the page instead of alert box )
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 
-], function (Controller, MessageToast, JSONModel) { // constructor function
+], function (Controller, MessageToast, JSONModel, ResourceModel) { // constructor function
 
     "use strict"; // helps detect coding issues, keeps things organised
 
@@ -18,11 +19,30 @@ sap.ui.define([
             };
             let oModel = new JSONModel(oData); // instance  of an class
             this.getView().setModel(oModel); // Question Mark these 2
+
+            // Set i18n on view
+
+            let i18nModel  = new ResourceModel({
+                bundleName : "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales : [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n")
         },
 
+
         onShowHello: function () {
-            // Show a native or vanilla JS alert
-            MessageToast.show("I love you")
+            // read message from i18n model
+            
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            let sMsg = oBundle.getText("helloMsg", [sRecipient]); // helloMsg its a variable from the i18n file 
+
+
+            // Show a native or vanilla JS alert instead of putting the message
+            // in the brakcets we added sMsg where we saved our name
+            // it represents the text from the box that appears at the end of the page
+            MessageToast.show(sMsg) 
         }
     });
 
