be89fc94207a3bbbeab3108b37c19fc8cfe09c62
Alexandru <anistor35@yahoo.com>
2023-09-11 14:04:08 +0300

    No longer needed but kept for educational purposes

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,14 @@
-sap.ui.define([
 
-    "sap/ui/core/mvc/XMLView"
+
+/* NO LONGER NEEDED FILE SINCE THE ADDITION OF component js and manifest changes
+
+
+sap.ui.define([
+  "sap/ui/core/mvc/XMLView" DELETED when we added the component js
+
+    "sap/ui/core/ComponentContainer"
     
-], function (XMLView) {
+], function (ComponentContainer) {
     "use strict";
 
     /* new Text({
@@ -10,10 +16,29 @@ sap.ui.define([
     }).placeAt("content")      without using the XML sepparate file
    */
 
+
+/*  This was used to place the view (our button + input) in the html body content
+    Removed it when the component.js file was created
+    STEP 9
+
     XMLView.create({
         viewName: "sap.ui.demo.walkthrough.view.App" 
     }).then( function (oView) {
         oView.placeAt("content");
     });
+
+
+
+    new ComponentContainer({
+        name : "sap.ui.demo.walkthrough",
+        settings : {
+            id : "walkthrough"
+
+        },
+        async : true
+    }).placeAt("content");
+
+
 });
 
+*/
