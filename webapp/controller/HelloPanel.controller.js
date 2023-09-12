22c65cfeb4941c41d43ec27f70ca438ff9cd45e3
Alexandru <anistor35@yahoo.com>
2023-09-12 14:11:24 +0300

    Controller for Panel.view

---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,54 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment" // load our module fragment
+
+], function(Controller, MessageToast, Fragment){
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+        
+        onShowHello: function () {
+
+            // read message from i18n model
+            
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            let sMsg = oBundle.getText("helloMsg", [sRecipient]); // helloMsg its a variable from the i18n file 
+    
+            MessageToast.show(sMsg) 
+        },
+
+        onOpenDialog : function (){
+            let oView = this.getView();
+
+            // Create the dialog lazily
+
+            if(!this.byId("helloDialog")){
+                //load asynchronous XML fragment
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                    controller: this
+
+                }).then(function(oDialog) {  // A PROMISE example.
+
+                    // connect dialog to the root view of this component (modules, lifecycle)
+                    
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                
+                })
+                
+            }else{
+                this.byId("helloDialog").open();
+            }
+
+        },
+
+        onCloseDialog : function () {
+            this.byId("helloDialog").close(); // 
+        }
+
+    })
+});
